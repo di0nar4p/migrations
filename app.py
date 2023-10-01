ef9663c4845c4410a8bf2c8f0315191c18d2389f
di0nar4p <glaucoplay@live.com>
2023-09-30 23:25:41 -0300

    estudos postgree

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ conexao= connect()
 conn= conexao.cursor()
 
 idade = 29
-query = (f"INSERT INTO Alunos (nome, idade) VALUES('Glauco',{idade})")
+'''query = (f"INSERT INTO Alunos (nome, idade) VALUES('Glauco',{idade})")
 conn.execute(query)
+'''
 conexao.commit()
 conn.execute("SELECT * FROM Alunos")
 registros= conn.fetchall()
