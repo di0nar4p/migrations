cd8d9c0dba326f55e3943ebbfb717d7083888d0a
di0nar4p <glaucoplay@live.com>
2023-10-01 13:37:21 -0300

    estudos

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ conexao= connect()
 conn= conexao.cursor()
 
 idade = 29
-'''query = (f"INSERT INTO Alunos (nome, idade) VALUES('Glauco',{idade})")
-conn.execute(query)
-'''
+#query = (f'''INSERT INTO Alunos (nome, idade) 
+#VALUES('Glauco',{idade})''')
+#conn.execute(query)
+
 conexao.commit()
 conn.execute("SELECT * FROM Alunos")
 registros= conn.fetchall()
 request=[]
 for row in registros:
-    print(row)
+    for i in range(len(row)):
+        request.append(row[i])
+
+print(request)
