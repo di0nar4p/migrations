65a920c7fd59f818a2816de80b0bb804d98b5370
di0nar4p <glaucoplay@live.com>
2023-09-30 21:53:20 -0300

    estudos postgree

---
+++ Controller/conexao.py
@@ -0,0 +1,17 @@
+import psycopg2
+import getpass as psw
+
+def connect():
+    try:
+        conn = psycopg2.connect(database = "estudos",
+                           host = "localhost",
+                           user = "postgres",
+                           password = 'root',
+                           port = 5432)
+    except Exception as error:
+        raise error('Deu ruim')
+    else:
+        
+        print('Sucessfully connected')
+        return conn
+
---
+++ app.py
@@ -0,0 +1,16 @@
+from Controller.conexao import connect
+
+
+conexao= connect()
+
+conn= conexao.cursor()
+
+idade = 29
+query = (f"INSERT INTO Alunos (nome, idade) VALUES('Glauco',{idade})")
+conn.execute(query)
+conexao.commit()
+conn.execute("SELECT * FROM Alunos")
+registros= conn.fetchall()
+request=[]
+for row in registros:
+    print(row)
