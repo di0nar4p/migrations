0e1fc052c19471946cb003e93ccc8c8524fe40a1
di0nar4p <glaucoplay@live.com>
2023-09-30 23:14:09 -0300

    estudos

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ conn= conexao.cursor()
 
 idade = 29
 query = (f"INSERT INTO Alunos (nome, idade) VALUES('Glauco',{idade})")
-#query = (f'''''')
 conn.execute(query)
 conexao.commit()
 conn.execute("SELECT * FROM Alunos")
